db80a99ede99c694098d197ebc7863555445958d
hipolito <huaylla7@hotmail.com>
2023-07-05 20:14:03 -0400

    Funcionando primer y segundo fragment

--- app/src/main/java/org/demre/mylogo/FragmentInicio.java
+++ app/src/main/java/org/demre/mylogo/FragmentInicio.java
@@ -3,12 +3,19 @@ package org.demre.mylogo;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import org.demre.mylogo.databinding.FragmentInicioBinding;
+
+import java.util.Objects;
 
 public class FragmentInicio extends Fragment {
 
@@ -16,8 +23,21 @@ public class FragmentInicio extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        FragmentInicioBinding binding = FragmentInicioBinding.inflate(getLayoutInflater());
-        return binding.getRoot();
+        FragmentInicioBinding binding = FragmentInicioBinding.inflate(inflater, container, false);
 
+        binding.btnSiguiente.setOnClickListener(v -> {
+            String datoIngresado = binding.textNombre.getEditText().getText().toString();
+
+            if (datoIngresado.isEmpty()){
+                Snackbar.make(v, "Debe ingresar un nombre", Snackbar.LENGTH_LONG).show();
+            }else {
+                Bundle envoltorio = new Bundle();
+            envoltorio.putString("key", datoIngresado);
+            Navigation.findNavController(v).navigate(R.id.action_fragmentInicio_to_fragmentPregunta, envoltorio);
+            }
+
+        });
+
+        return binding.getRoot();
     }
 }
--- app/src/main/java/org/demre/mylogo/FragmentPregunta.java
+++ app/src/main/java/org/demre/mylogo/FragmentPregunta.java
@@ -1,24 +1,33 @@
 package org.demre.mylogo;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import org.demre.mylogo.databinding.FragmentInicioBinding;
 import org.demre.mylogo.databinding.FragmentPreguntaBinding;
 
 public class FragmentPregunta extends Fragment {
 
+    private String name;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         FragmentPreguntaBinding binding = FragmentPreguntaBinding.inflate(inflater, container, false);
+
+        String nombre = getArguments().getString("key");
+        binding.txtObtenido.setText("Hola " + nombre);
+
+        /*
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            name = bundle.getString("name");
+        }
+        // Display the name in the textView
+        binding.txtObtenido.setText("HOLA " + name);*/
+
         return binding.getRoot();
     }
 }
